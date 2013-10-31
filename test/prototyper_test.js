4b00d47d84f5b2364846dc5a6d7d8412f7b63aa4
operandom <dev@operandom.com>
2013-10-31 16:34:03 +0100

    Add some tests.

--- test/prototyper_test.js
+++ test/prototyper_test.js
@@ -1,36 +1,110 @@
 'use strict';
 
-var prototyper = require('../lib/prototyper.js');
+var prototyper = require('../lib/prototyper.js'),
+	util = require('util'),
+	events = require('events'),
+	instance;
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+function ClassTest() {}
+
 
 exports['prototyper'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(prototyper.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  }
+	setUp: function(done) {
+		
+		
+		util.inherits(ClassTest, events.EventEmitter);
+		
+		prototyper
+		.define(ClassTest.prototype)
+		.c('CONST', 'const')
+		.p('var', 'var')
+		.p('bool', true).type(Boolean)
+		.p('boolE', true).type(Boolean).e()
+		.p('number', 3.14).type(Number)
+		.p('numberE', 3.14).type(Number).e()
+		.p('string', 'string').type(String)
+		.p('stringE', 'stringE').type(String).e()
+		;
+		
+		instance = new ClassTest;
+		
+		done();
+	},
+	'no args': function(test) {
+		
+		test.expect(17);
+		
+		
+		// BASE
+		test.ok(instance);
+		test.strictEqual(instance.CONST, 'const');
+		test.strictEqual(instance.var, 'var');
+		
+		
+		// BOOLEAN
+		test.strictEqual(instance.bool, true);
+		test.doesNotThrow(
+			function() {
+				instance.bool = false;
+				instance.bool = true;
+			},
+			'A boolean should be assigned.'
+		);
+		test.throws(
+			function() {
+				instance.bool = 0;
+			},
+			"A Number shouldn't be assigned to a Boolean."
+		);
+		
+		test.strictEqual(instance.boolE, true);
+		test.doesNotThrow(
+			function() {
+				instance.boolE = false;
+			},
+			'A boolean should be assigned.'
+		);
+		test.strictEqual(instance.boolE, false);
+		test.throws(
+			function() {
+				instance.boolE = 0;
+			},
+			"A Number shouldn't be assigned to a Boolean."
+		);
+		
+		
+		// NUMBER
+		var number = 3.14;
+		test.strictEqual(instance.number, number, instance.number + ' should be equal to ' + number);
+		
+		number = new Number(Math.LN2);
+		test.doesNotThrow(
+			function() {
+				instance.number = number;
+			},
+			number + ' should be assigned to instance.number.'
+		);
+		test.strictEqual(instance.number, number, instance.number + ' should be equal to ' + number);
+		test.throws(
+			function() {
+				instance.number = true;
+			},
+			"A Boolean shouln't be assigned to instance.number"
+		);
+		
+		
+		// STRING
+		var string = 'string2';
+		test.strictEqual(instance.string, 'string', instance.string + ' should be equal to "string".');
+		test.doesNotThrow(
+			function() {
+				instance.string = string;
+			},
+			string + ' should be assigned to instance.string'
+		);
+		test.strictEqual(instance.string, string, instance.string + ' should be equal to ' + string);
+		
+		test.done();
+	}
 };
